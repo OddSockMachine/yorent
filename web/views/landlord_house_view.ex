9d4fccd1257cec117f6f242c12051151eba446e3
David Walker <david.walker@anaplan.com>
2016-02-20 17:11:32 +0000

    making house list view more presentable

--- web/views/landlord_house_view.ex
+++ web/views/landlord_house_view.ex
@@ -1,3 +1,11 @@
 defmodule Yorent.LandlordHouseView do
   use Yorent.Web, :view
+  def view_price(p) do
+    p_str = Integer.to_string(p)
+    len = String.length(p_str)
+    pounds = String.slice(p_str, 0,  len - 2)
+    pence = String.slice(p_str, len - 2,  len)
+    "£" <> pounds <> "." <> pence
+  end
+
 end
